ec340e6fdb52e8a0beb7fc34c7eb262726e46bb9
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-30 14:51:44 +0700

    commit4

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -27,5 +27,19 @@ public:
             cout << "\nqueue overflow\n";
             return;
         }
+
+
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            
+            if (REAR == max - 1)
+                REAR = 0;
+            else 
+            REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
