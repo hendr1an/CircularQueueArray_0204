22edbab71bb32e8fe048cc1b186787014c41b4b0
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-30 15:37:04 +0700

    commit10

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -100,6 +100,25 @@ public:
                 cout << queue_array[FRONT_Position] << "    ";
                 FRONT_Position++;
             }
+            cout << endl;
         }
     }
 };
+
+int main () {
+    Queues q;
+    char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+        }
+    }
+}
